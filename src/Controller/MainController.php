3b21340f5e1fd613f017ad3965b9d2829473a521
emmapreuvot <emma.preuvot2023@campus-eni.fr>
2024-02-20 13:50:45 +0100

    amélioration jusqu'au module 7

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use JetBrains\PhpStorm\NoReturn;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+use function MongoDB\BSON\fromJSON;
 
 class MainController extends AbstractController
 {
@@ -18,7 +20,19 @@ class MainController extends AbstractController
     #[Route('/test', name: 'app_test', methods: ['GET','POST'])]
     public function test(): \Symfony\Component\HttpFoundation\Response
     {
-        return $this->render('main/test.html.twig');
+
+        $serie = ["title" => "Game of thrones", "year"=> 2013];
+
+        return $this->render('main/test.html.twig', ["mySerie"=>$serie]);
+    }
+
+    #[Route('/aboutus', name: 'app_about_us', methods: ['GET','POST'])]
+    public function aboutus(SerializerInterface $serializer): \Symfony\Component\HttpFoundation\Response
+    {
+        $filePath = $this->getParameter('kernel.project_dir') . '/data/team.json';
+        $jsonContent = file_get_contents($filePath);
+        $aboutus = $serializer->decode($jsonContent, 'json');;
+        return $this->render('main/aboutus.html.twig',['aboutus'=>$aboutus]);
     }
 
 }
